7ed2d7574912b5c8c74f70e81ccdbf1e189fa605
ba1yya <34457638+Andrey5800@users.noreply.github.com>
2023-07-23 18:32:25 +0300

    update v3

--- app.py
+++ app.py
@@ -1,35 +1,57 @@
 import gradio as gr
 from refacer import Refacer
 import argparse
-import multiprocessing as mp
-import os
+import ngrok
 
 parser = argparse.ArgumentParser(description='Refacer')
-parser.add_argument("--max_num_faces", help="Max number of faces on UI", type=int, default=8)
-parser.add_argument("--force_cpu", help="Force CPU mode", default=False,action="store_true")
-parser.add_argument("--share_gradio", help="Share Gradio", default=True,action="store_true")
-parser.add_argument("--autolaunch", help="Auto start in browser", default=False,action="store_true")
-parser.add_argument("--server_name", help="Server IP address", default="127.0.0.1")
-parser.add_argument("--server_port", help="Server port", type=int, default=7860)
-parser.add_argument("--tensorrt", help="TensorRT activate", default=True,action="store_true")
-parser.add_argument("--gpu-threads", help="number of threads to be use for the GPU", dest="gpu_threads", type=int, default=10)
-parser.add_argument('--max-memory', help='maximum amount of RAM in GB to be used', dest='max_memory', type=int, default=10000)
-parser.add_argument('--video_quality', help='настроить качество выходного видео', dest='video_quality', type=int, default=30, choices=range(52), metavar='[0-51]')
-#parser.add_argument("--mem", help="Max memory", dest="cpu_threads", type=int, default=4)
-
+parser.add_argument("--max_num_faces", type=int, help="Max number of faces on UI", default=5)
+parser.add_argument("--force_cpu", help="Force CPU mode", default=False, action="store_true")
+parser.add_argument("--share_gradio", help="Share Gradio", default=False, action="store_true")
+parser.add_argument("--server_name", type=str, help="Server IP address", default="127.0.0.1")
+parser.add_argument("--server_port", type=int, help="Server port", default=7860)
+parser.add_argument("--colab_performance", help="Use in colab for better performance", default=False,action="store_true")
+parser.add_argument("--ngrok", type=str, help="Use ngrok", default=None)
+parser.add_argument("--ngrok_region", type=str, help="ngrok region", default="us")
 args = parser.parse_args()
 
-refacer = Refacer(force_cpu=args.force_cpu,tensorrt=args.tensorrt,gpu_threads=args.gpu_threads,max_memory=args.max_memory,video_quality=args.video_quality)
+refacer = Refacer(force_cpu=args.force_cpu,colab_performance=args.colab_performance)
 
 num_faces=args.max_num_faces
+
+# Connect to ngrok for ingress
+def connect(token, port, options):
+    account = None
+    if token is None:
+        token = 'None'
+    else:
+        if ':' in token:
+            # token = authtoken:username:password
+            token, username, password = token.split(':', 2)
+            account = f"{username}:{password}"
+
+    # For all options see: https://github.com/ngrok/ngrok-py/blob/main/examples/ngrok-connect-full.py
+    if not options.get('authtoken_from_env'):
+        options['authtoken'] = token
+    if account:
+        options['basic_auth'] = account
+
+
+    try:
+        public_url = ngrok.connect(f"127.0.0.1:{port}", **options).url()
+    except Exception as e:
+        print(f'Invalid ngrok authtoken? ngrok connection aborted due to: {e}\n'
+              f'Your token: {token}, get the right one on https://dashboard.ngrok.com/get-started/your-authtoken')
+    else:
+        print(f'ngrok connected to localhost:{port}! URL: {public_url}\n'
+               'You can use this link after the launch is complete.')
+
 
 def run(*vars):
     video_path=vars[0]
     origins=vars[1:(num_faces+1)]
     destinations=vars[(num_faces+1):(num_faces*2)+1]
     thresholds=vars[(num_faces*2)+1:]
-    upscaler=vars[-1]
-    
+
     faces = []
     for k in range(0,num_faces):
         if origins[k] is not None and destinations[k] is not None:
@@ -39,43 +61,33 @@ def run(*vars):
                 'threshold':thresholds[k]
             })
 
-    # Преобразование upscaler в строку
-    return refacer.reface(video_path,faces,str(upscaler))
+    return refacer.reface(video_path,faces)
 
 origin = []
 destination = []
 thresholds = []
-upscaler = []
-upscaler = []
-upscaler_models = ['None']
-upscaler_models += [file for file in os.listdir('upscaler_models') if file.endswith('.onnx')]
-print(upscaler_models)
 
 with gr.Blocks() as demo:
     with gr.Row():
-        gr.Markdown("# Refacer | Repack от https://www.youtube.com/@ba1yya")
+        gr.Markdown("# Refacer")
     with gr.Row():
-        video=gr.Video(label="Оригинальное видео",format="mp4")
-        video2=gr.Video(label="Обработанное видео",interactive=False,format="mp4")
+        video=gr.Video(label="Original video",format="mp4")
+        video2=gr.Video(label="Refaced video",interactive=False,format="mp4")
 
     for i in range(0,num_faces):
-        with gr.Tab(f"Лицо #{i+1}"):
+        with gr.Tab(f"Face #{i+1}"):
             with gr.Row():
-                origin.append(gr.Image(label="Лицо, которое заменяем"))
-                destination.append(gr.Image(label="Лицо, на которое надо заменить"))
+                origin.append(gr.Image(label="Face to replace"))
+                destination.append(gr.Image(label="Destination face"))
             with gr.Row():
-                thresholds.append(gr.Slider(label="Порог",minimum=0.0,maximum=1.0,value=0.2))
+                thresholds.append(gr.Slider(label="Threshold",minimum=0.0,maximum=1.0,value=0.2))
     with gr.Row():
-        #upscaler.append(gr.Radio(label="Upscaler", choices=models_ESRGAN, value=models_ESRGAN[0], interactive=True))
-        upscaler.append(gr.Dropdown(label="Выберите модель апскейлера", choices=upscaler_models, value=upscaler_models[0], interactive=True))
-    with gr.Row():
-        button=gr.Button("Начать обработку", variant="primary")
+        button=gr.Button("Reface", variant="primary")
 
-    button.click(fn=run,inputs=[video]+origin+destination+thresholds+upscaler,outputs=[video2])
+    button.click(fn=run,inputs=[video]+origin+destination+thresholds,outputs=[video2])
+    
+if args.ngrok is not None:
+    connect(args.ngrok, args.server_port, {'region': args.ngrok_region, 'authtoken_from_env': False})
     
 #demo.launch(share=True,server_name="0.0.0.0", show_error=True)
-demo.queue().launch(show_error=True,share=args.share_gradio,server_name=args.server_name,inbrowser=args.autolaunch)
-
-#demo.launch(share=True,server_name="0.0.0.0", show_error=True)
-#demo.queue().launch(show_error=True,share=False,inbrowser=True)
-#e().launch(show_error=True,share=False,inbrowser=True)
+demo.queue().launch(show_error=True,share=args.share_gradio,server_name=args.server_name,server_port=args.server_port)
